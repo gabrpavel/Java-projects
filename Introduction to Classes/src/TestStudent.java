73e0774e756c3d462eb1d8cea6d2a307284c557b
gabrpavel <110068322+gabrpavel@users.noreply.github.com>
2023-05-21 00:02:12 +0300

    Update TestStudent.java

--- Introduction to Classes/src/TestStudent.java
+++ Introduction to Classes/src/TestStudent.java
@@ -8,9 +8,9 @@ public class TestStudent {
 
                 students[0] = new Student("Pavel", 18, 7.8F);
                 students[1] = new Student("Vlad", 18, 8.8F);
-                students[2] = new Student("Vlad", 18, 6.9F);
-                students[3] = new Student("Kirill", 17, 7.9F);
-                students[4] = new Student("Pasha", 17, 6.5F);
+                students[2] = new Student("Vlad", 18, 7.9F);
+                students[3] = new Student("Kirill", 17, 8.2F);
+                students[4] = new Student("Pasha", 17, 7.5F);
 
                 for (int i = 0; i < students.length; i++)
                         System.out.printf("Student %d: %s, %d years, GPA %f\n",
